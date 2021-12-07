c85e80be1efcbf1ef7a97f34d93210f3c651c291
MARION Damien <damien.marion@irisa.fr>
2021-12-07 17:52:26 +0100

    remove localhost

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-source=$1
 
-cp -R ${source}/*  .
-rm token
+cp -R ../personal-website/_site/* .
+rm token.txt
 
 sed -i 's/localhost\:4000/https\:\/\/damien\-marion\.github\.io/g' $(find . -type f)
